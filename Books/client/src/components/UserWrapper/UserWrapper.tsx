e7dc76f16eaec58f7c7f069a243414ad8002ae25
Dmitry Kulakov <atomeistee@gmail.com>
2022-08-30 10:45:58 +0300

    refactor: use router-dom outlet
    That allows to not import UserWrapper each time when we are using it in users pages, and outlet context provides children pages with user data

--- Books/client/src/components/UserWrapper/UserWrapper.tsx
+++ Books/client/src/components/UserWrapper/UserWrapper.tsx
@@ -1,17 +1,35 @@
 import './UserWrapper.scss';
 import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, Outlet } from 'react-router-dom';
 
 import { ReactComponent as BookListIcon } from '../../assets/images/booklist-icon.svg';
 import { ReactComponent as BookmarkIcon } from '../../assets/images/bookmark-icon.svg';
 import { ReactComponent as HomeIcon } from '../../assets/images/home-icon.svg';
 import { ReactComponent as LogOutIcon } from '../../assets/images/log-out-icon.svg';
 
-type UserWrapperProps = {
-  children: React.ReactElement;
+const mockUser = {
+  id: 'id',
+  username: 'AtomEistee',
+  currentBooks: [
+    {
+      title: 'Atomic Habits',
+      authors: 'Carl Newport',
+      cover: 'link/to/cover.png',
+      category: ['Fantasy', 'Drama'],
+      description: 'Amazing description of amazing book, but who really knows?...',
+      publisher: 'Publisher Name',
+      publisherYear: 1920,
+      id: 'UniqueID',
+      pages: 89,
+      isReading: true,
+      isInCollection: false,
+    },
+  ],
 };
 
-export const UserWrapper = ({ children }: UserWrapperProps) => {
+export const UserWrapper = () => {
+  // there I thought making a request to server by that username, to get current user or something like that
+  const user = mockUser;
   const { username } = useParams();
   return (
     <div className="userWrapper">
@@ -22,7 +40,9 @@ export const UserWrapper = ({ children }: UserWrapperProps) => {
           type={'search'}
         />
       </header>
-      <main className="userMain">{children}</main>
+      <main className="userMain">
+        <Outlet context={user} />
+      </main>
       <footer className="userFooter">
         <nav className="userNavigation">
           <li className="navigationLink">
