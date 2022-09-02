e6d76508f914828e849b21c1462328251e323551
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-02 21:54:21 +0200

    deleting is_reading and bookmarked replace references from id to book_id

--- Books/server/migrations/sqls/20220823104701-user-books-table-up.sql
+++ Books/server/migrations/sqls/20220823104701-user-books-table-up.sql
@@ -1,9 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE user_books
 (
-    id         uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
-    user_id    uuid REFERENCES users (id),
-    book_id    uuid REFERENCES books (id),
-    is_reading BOOLEAN NOT NULL,
-    bookmarked BOOLEAN NOT NULL
+    id      uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    user_id uuid REFERENCES users (id),
+    book_id BIGINT REFERENCES books (book_id)
 );
