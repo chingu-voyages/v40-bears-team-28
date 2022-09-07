0b46bef35484a632e33a4fa86a7a129bd9ab6fa1
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-07 23:58:54 +0200

    created useLogout hook to logout user

---
+++ Books/client/src/hooks/useLogout.ts
@@ -0,0 +1,42 @@
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { deleteUserSession } from "../api/users.api";
+import { AuthContext, AuthUser } from "../context/auth.context";
+
+type UseLogoutReturn = {
+  logoutUser: () => void;
+};
+export default function useLogout(): UseLogoutReturn {
+  const { user, setUser } = useContext(AuthContext);
+  const [logout, setLogout] = useState(false);
+  const navigate = useNavigate();
+
+  function logoutUser(): void {
+    setLogout(true);
+  }
+
+  useEffect(() => {
+    const controller = new AbortController();
+    if (logout) {
+      deleteUserSession({ controller, token: user.token })
+        .then(() => {
+          setUser({} as AuthUser);
+          setLogout(false);
+          navigate("/");
+        })
+        .catch(() => {
+          setUser({} as AuthUser);
+          setLogout(false);
+          navigate("/");
+        });
+    }
+    return () => {
+      controller.abort();
+    };
+  }, [logout]);
+
+  return {
+    logoutUser,
+  };
+}
