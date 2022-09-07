4c503900be08c0f1b3544e6810ab4661a0707afe
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-08 00:05:30 +0200

    updating book_id to be string

--- Books/server/src/models/user_books.model.ts
+++ Books/server/src/models/user_books.model.ts
@@ -1,82 +1,82 @@
-import database from '../database';
+import database from '../database'
 
 type UserBook = {
   id?: string
   user_id: string
-  book_id: number
+  book_id: string
 }
 
 export class UserBooksModel {
   async index(): Promise<UserBook[]> {
     try {
-      const connect = await database.connect();
+      const connect = await database.connect()
       const sql = `SELECT *
-                   FROM user_books`;
-      const results = await connect.query(sql);
-      connect.release();
-      return results.rows;
+                   FROM user_books`
+      const results = await connect.query(sql)
+      connect.release()
+      return results.rows
     } catch (error) {
-      throw new Error(`Unable to get all user books, ${(error as Error).message}`);
+      throw new Error(`Unable to get all user books, ${(error as Error).message}`)
     }
   }
 
   async showByUserId(id: string): Promise<UserBook[]> {
     try {
-      const connect = await database.connect();
-      const sql = `SELECT *
+      const connect = await database.connect()
+      const sql = `SELECT b.book_id AS id, b.authors, b.image, b.title, b.description, b.url
                    FROM user_books
                             INNER JOIN books b on b.book_id = user_books.book_id
-                   WHERE user_id = $1`;
-      const results = await connect.query(sql, [id]);
-      connect.release();
-      return results.rows;
+                   WHERE user_id = $1`
+      const results = await connect.query(sql, [id])
+      connect.release()
+      return results.rows
     } catch (error) {
-      throw new Error(`Unable to get all all user books by user id, ${(error as Error).message}`);
+      throw new Error(`Unable to get all all user books by user id, ${(error as Error).message}`)
     }
   }
 
-  async showByUserIdAndBookId(userId: string, bookId: number): Promise<UserBook> {
+  async showByUserIdAndBookId(userId: string, bookId: string): Promise<UserBook> {
     try {
-      const connect = await database.connect();
+      const connect = await database.connect()
       const sql = `SELECT *
                    FROM user_books
                    WHERE user_id = $1
-                     AND book_id = $2`;
-      const results = await connect.query(sql, [userId, bookId]);
-      connect.release();
-      return results.rows[0];
+                     AND book_id = $2`
+      const results = await connect.query(sql, [userId, bookId])
+      connect.release()
+      return results.rows[0]
     } catch (error) {
-      throw new Error(`Unable to get user book by user id and book id, ${(error as Error)}`);
+      throw new Error(`Unable to get user book by user id and book id, ${error as Error}`)
     }
   }
 
-  async create(userId: string, bookId: number): Promise<UserBook> {
+  async create(userId: string, bookId: string): Promise<UserBook> {
     try {
-      const connect = await database.connect();
+      const connect = await database.connect()
       const sql = `INSERT INTO user_books (user_id, book_id)
                    VALUES ($1, $2)
-                   RETURNING *`;
-      const results = await connect.query(sql, [userId, bookId]);
-      connect.release();
-      return results.rows[0];
+                   RETURNING *`
+      const results = await connect.query(sql, [userId, bookId])
+      connect.release()
+      return results.rows[0]
     } catch (error) {
-      throw new Error(`Unable to create user book, ${(error as Error).message}`);
+      throw new Error(`Unable to create user book, ${(error as Error).message}`)
     }
   }
 
-  async delete(userId: string, bookId: number): Promise<UserBook> {
+  async delete(userId: string, bookId: string): Promise<UserBook> {
     try {
-      const connect = await database.connect();
+      const connect = await database.connect()
       const sql = `DELETE
                    FROM user_books
                    WHERE user_id = $1
                      AND book_id = $2
-                   RETURNING *`;
-      const results = await connect.query(sql, [userId, bookId]);
-      connect.release();
-      return results.rows[0];
+                   RETURNING *`
+      const results = await connect.query(sql, [userId, bookId])
+      connect.release()
+      return results.rows[0]
     } catch (error) {
-      throw new Error(`Unable to delete user book, ${(error as Error).message}`);
+      throw new Error(`Unable to delete user book, ${(error as Error).message}`)
     }
   }
 }
