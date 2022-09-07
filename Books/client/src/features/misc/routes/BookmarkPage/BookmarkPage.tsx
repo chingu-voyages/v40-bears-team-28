228482ad72b84356e181ce7cec34af287d200a4f
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-07 23:51:04 +0200

    created BookmarkPage to get all saved books for a user

---
+++ Books/client/src/features/misc/routes/BookmarkPage/BookmarkPage.tsx
@@ -0,0 +1,55 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { getSavedBooksForUser } from "../../../../api/user_books.api";
+import { BookOverview } from "../../../../components/BookOverview";
+import { Head } from "../../../../components/Head/Head";
+import { AuthContext } from "../../../../context/auth.context";
+import useWindowSize from "../../../../hooks/useWindowSize";
+import { Book } from "../Types";
+
+export function BookmarkPage() {
+  const { user } = useContext(AuthContext);
+  const [books, setBooks] = useState<Book[]>([]);
+  const { width } = useWindowSize();
+  useEffect(() => {
+    const controller = new AbortController();
+
+    async function fetchBook() {
+      const books = await getSavedBooksForUser({ controller, userId: user.id, token: user.token });
+      setBooks(books);
+    }
+
+    fetchBook().catch(() => {
+      setBooks([]);
+    });
+    return () => {
+      controller.abort();
+    };
+  }, []);
+  return (
+    <div className="contentWrapper">
+      <Head description={`${user.username} library`} title={`${user.username}`} />
+      <h1 className="userPageTitle">Saved Books</h1>
+      {books.length ? (
+        <Swiper
+          slidesPerView={width > 760 ? 2 : 1}
+          simulateTouch={true}
+          grabCursor={true}
+          className="swiper"
+          modules={[Navigation]}
+          navigation={true}
+        >
+          {books.map(({ title, image, id, authors }: Book) => (
+            <SwiperSlide className="swiperSlide" key={id}>
+              <BookOverview title={title} image={image} authors={authors} id={id} key={id} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      ) : (
+        <h2>No Saved books</h2>
+      )}
+    </div>
+  );
+}
