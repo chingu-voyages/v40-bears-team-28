514eebffdde2f7f5c21268d11f01edd81eeccdd8
Dmitry Kulakov <atomeistee@gmail.com>
2022-09-08 12:46:02 +0300

    feat: style authentication pages

--- Books/client/src/features/Login/index.tsx
+++ Books/client/src/features/Login/index.tsx
@@ -2,11 +2,17 @@ import { useState } from "react";
 
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import "./styles/login.scss";
 
 function Login() {
   // To show sign in page if login is true and show sign up page if login is false
   const [login, setLogin] = useState(true);
-  return <div>{login ? <SignIn setLogin={setLogin} /> : <SignUp setLogin={setLogin} />}</div>;
+  return (
+    <div className="auth-page">
+      <div className="background-square"></div>
+      {login ? <SignIn setLogin={setLogin} /> : <SignUp setLogin={setLogin} />}
+    </div>
+  );
 }
 
 export default Login;
