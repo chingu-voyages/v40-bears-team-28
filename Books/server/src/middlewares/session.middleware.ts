a21fa6760bdcaa5d49cc230256d44b356130ef66
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-12 16:45:00 +0200

    updated cors config and redis-host

--- Books/server/src/middlewares/session.middleware.ts
+++ Books/server/src/middlewares/session.middleware.ts
@@ -1,17 +1,20 @@
-import session, { SessionData } from 'express-session';
-import Redis from 'ioredis';
-import connectRedis from 'connect-redis';
-import config from '../config';
+import session, { SessionData } from 'express-session'
+import Redis from 'ioredis'
+import connectRedis from 'connect-redis'
+import config from '../config'
 
 // Adding user type to session
 declare module 'express-session' {
   interface SessionData {
-    user?: any;
+    user?: any
   }
 }
 
-export const redisClient = new Redis();
-const RedisStore = connectRedis(session);
+export const redisClient = new Redis({
+  host: config.redisHost as string,
+  port: config.redisPort as unknown as number
+})
+const RedisStore = connectRedis(session)
 
 export const sessionMiddleware = session({
   secret: config.cookieSecret as string,
@@ -23,11 +26,11 @@ export const sessionMiddleware = session({
     maxAge: 1000 * 60 * 60 * 24 * 7,
     secure: config.env === 'production ' ? true : 'auto',
     httpOnly: true,
-    sameSite: config.env === 'production' ? 'none' : 'lax',
-  },
-});
+    sameSite: config.env === 'production' ? 'none' : 'lax'
+  }
+})
 
 export const corsConfig = {
-  origin: 'http://localhost:3000',
-  credentials: true,
-};
+  origin: 'https://books-hut.netlify.app/',
+  credentials: true
+}
